fc8667310c95c555a6de1d16f228e9e970abe910
Thomas Orozco <thomas@orozco.fr>
2014-03-26 23:30:00 +0100

    Make the username and host arguments

--- src/main/java/com/scalr/SSHLauncher.java
+++ src/main/java/com/scalr/SSHLauncher.java
@@ -4,42 +4,55 @@ import com.scalr.exception.EnvironmentSetupException;
 import com.scalr.exception.InvalidEnvironmentException;
 import com.scalr.launcher.MacOSLauncher;
 import com.scalr.launcher.SSHLauncherInterface;
+import org.apache.commons.cli.*;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 
 
 public class SSHLauncher {
+    public static void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("ssh-launcher", options);
+        System.exit(1);
+    }
 
-    public static void main(String args[]) {
-        //String[] command = {"/bin/bash", "-c", "ssh", "orozco.fr"};
-        System.out.println("Launching SSH Session");
+    public static void main(String args[]) throws IOException, EnvironmentSetupException, InterruptedException, InvalidEnvironmentException {
+        Options options = new Options();
+        options.addOption("u", "username", true, "SSH user to login as");
+        options.addOption("h", "host", true, "Host to connect to");
+        CommandLineParser parser = new GnuParser();
 
-        SSHConfiguration sshConfiguration = new SSHConfiguration("root", "orozco.fr");
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            printHelp(options);
+            System.exit(1);
+        }
+
+        String username = cmd.getOptionValue("username");
+        String host     = cmd.getOptionValue("host");
+
+        if (username == null || host == null)  {
+            printHelp(options);
+            System.exit(1);
+        }
+
+
+        SSHConfiguration sshConfiguration = new SSHConfiguration(cmd.getOptionValue("username"),cmd.getOptionValue("host"));
         SSHLauncherInterface launcher = new MacOSLauncher();
 
-        try {
-            launcher.setUpEnvironment(sshConfiguration);
-        } catch (EnvironmentSetupException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        try {
-            String sshCommand[] = launcher.getSSHCommand();
-            System.out.println(StringUtils.join(sshCommand, " "));
+        launcher.setUpEnvironment(sshConfiguration);
+        String sshCommand[] = launcher.getSSHCommand();
 
-            ProcessBuilder pb = new ProcessBuilder().inheritIO().command(sshCommand);
-            Process p = pb.start();
-            p.waitFor();
-        } catch (InvalidEnvironmentException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Launching SSH Session");
+        System.out.println(StringUtils.join(sshCommand, " "));
+
+        ProcessBuilder pb = new ProcessBuilder().inheritIO().command(sshCommand);
+        Process p = pb.start();
+        p.waitFor();
 
         launcher.tearDownEnvironment();
 
