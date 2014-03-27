8b1974a806793f88c56f29e9b7517272af11e47b
Thomas Orozco <thomas@orozco.fr>
2014-03-27 14:39:21 +0100

    Add Applet for browser launch

--- src/main/java/com/scalr/SSHLauncher.java
+++ src/main/java/com/scalr/SSHLauncher.java
@@ -17,6 +17,22 @@ public class SSHLauncher {
         System.exit(1);
     }
 
+    public static void launchSSHFromConfiguration(SSHConfiguration sshConfiguration) throws IOException, EnvironmentSetupException, InvalidEnvironmentException, InterruptedException {
+        SSHLauncherInterface launcher = new MacOSLauncher();
+
+        launcher.setUpEnvironment(sshConfiguration);
+        String sshCommand[] = launcher.getSSHCommand();
+
+        System.out.println("Launching SSH Session");
+        System.out.println(StringUtils.join(sshCommand, " "));
+
+        ProcessBuilder pb = new ProcessBuilder().inheritIO().command(sshCommand);
+        Process p = pb.start();
+        p.waitFor();
+
+        launcher.tearDownEnvironment();
+    }
+
     public static void main(String args[]) throws IOException, EnvironmentSetupException, InterruptedException, InvalidEnvironmentException {
         Options options = new Options();
         options.addOption("u", "username", true, "SSH user to login as");
@@ -39,22 +55,8 @@ public class SSHLauncher {
             System.exit(1);
         }
 
-
         SSHConfiguration sshConfiguration = new SSHConfiguration(cmd.getOptionValue("username"),cmd.getOptionValue("host"));
-        SSHLauncherInterface launcher = new MacOSLauncher();
-
-
-        launcher.setUpEnvironment(sshConfiguration);
-        String sshCommand[] = launcher.getSSHCommand();
-
-        System.out.println("Launching SSH Session");
-        System.out.println(StringUtils.join(sshCommand, " "));
-
-        ProcessBuilder pb = new ProcessBuilder().inheritIO().command(sshCommand);
-        Process p = pb.start();
-        p.waitFor();
-
-        launcher.tearDownEnvironment();
+        launchSSHFromConfiguration(sshConfiguration);
 
         System.out.println("Exiting");
     }
---
+++ src/main/java/com/scalr/SSHLauncherApplet.java
@@ -0,0 +1,73 @@
+package com.scalr;
+
+import com.scalr.exception.EnvironmentSetupException;
+import com.scalr.exception.InvalidEnvironmentException;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.io.IOException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+public class SSHLauncherApplet extends Applet {
+
+    StringBuffer buffer;
+
+    public void init() {
+        buffer = new StringBuffer();
+        addItem("initializing... ");
+    }
+
+    public void start() {
+        addItem("starting... ");
+        addItem("user");
+        addItem(getParameter("user"));
+        addItem("host");
+        addItem(getParameter("host"));
+
+        String user = getParameter("user");
+        String host = getParameter("host");
+
+        final SSHConfiguration sshConfiguration = new SSHConfiguration(user, host);
+
+            AccessController.doPrivileged(new PrivilegedAction() {
+                public Object run() {
+                    try {
+                        SSHLauncher.launchSSHFromConfiguration(sshConfiguration);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (EnvironmentSetupException e) {
+                        e.printStackTrace();
+                    } catch (InvalidEnvironmentException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+            });
+    }
+
+    public void stop() {
+        addItem("stopping... ");
+    }
+
+    public void destroy() {
+        addItem("preparing for unloading...");
+    }
+
+    void addItem(String newWord) {
+        System.out.println(newWord);
+        buffer.append(newWord);
+        buffer.append("\n");
+        repaint();
+    }
+
+    public void paint(Graphics g) {
+        //Draw a Rectangle around the applet's display area.
+        g.drawRect(0, 0, size().width - 1, size().height - 1);
+
+        //Draw the current string inside the rectangle.
+        g.drawString(buffer.toString(), 5, 15);
+    }
+}
