3898cd3744aa4de90abf0bff729020b04fe7c6e9
Thomas Orozco <thomas@orozco.fr>
2015-05-06 17:08:35 +0200

    Cleanup mac imports

--- src/main/java/com/scalr/ssh/launcher/mac/MacAppSystemObserver.java
+++ src/main/java/com/scalr/ssh/launcher/mac/MacAppSystemObserver.java
@@ -1,14 +1,13 @@
 package com.scalr.ssh.launcher.mac;
 
-import com.apple.eawt.*;
-import com.scalr.ssh.launcher.SSHLauncher;
+import com.apple.eawt.AppEvent;
+import com.apple.eawt.Application;
+import com.apple.eawt.OpenURIHandler;
 import com.scalr.ssh.launcher.configuration.LauncherConfigurationInterface;
 import com.scalr.ssh.launcher.configuration.UriLauncherConfiguration;
 import com.scalr.ssh.logging.Loggable;
 
 import java.net.URI;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MacAppSystemObserver extends Loggable implements OpenURIHandler, MacAppViewInterface {
     private final MacAppController appController;
